fe95ce2c2c735820d8d46dcc93bd4fe1edc84f6e
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-06-12 07:11:45 -0300

    :sparkles: feat(index.js): Create file exporting functions to help with field check and calculation

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,2 +1,26 @@
-const code = "";
-export default code;
+const IndexFunctions = {
+  checkQuantityChar(char) {
+    if (String(char).length > 11) {
+      throw "Too big";
+    }
+  },
+  doMath(text) {
+    if (!text.trim()) {
+      return "Try again";
+    }
+    try {
+      text = eval(text.trim());
+      this.checkQuantityChar(text);
+    } catch (error) {
+      if (error == "Too big") {
+        text = error;
+        return;
+      }
+      text = "Try again";
+    } finally {
+      return text;
+    }
+  },
+};
+
+export default IndexFunctions;
