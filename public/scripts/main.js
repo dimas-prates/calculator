a15a3337d760c472bb517ccb2dc82909bf098527
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-06-12 10:19:45 -0300

    :sparkles: feat(main.js): Add event listener in a loop for all buttons and improved insert function

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,7 @@
 import IndexFunctions from "./index.js";
 
 const elementSpanResult = document.querySelector("[data-result]");
+
 let resultField = elementSpanResult.innerHTML;
 
 function clearInfo() {
@@ -9,7 +10,18 @@ function clearInfo() {
 }
 function insertChar(char) {
   resultField += char.trim();
-  elementSpanResult.innerHTML = resultField.trim();
+  try {
+    IndexFunctions.checkQuantityChar(resultField);
+  } catch (error) {
+    if (error == "Too big") {
+      resultField = error;
+      return;
+    }
+    resultField = "Try again";
+  } finally {
+    elementSpanResult.innerHTML = resultField.trim();
+    return elementSpanResult.innerHTML;
+  }
 }
 function eraseChar() {
   resultField = resultField.slice(0, resultField.length - 1).trim();
@@ -19,4 +31,26 @@ function eraseChar() {
 function getResult() {
   resultField = IndexFunctions.doMath(resultField);
   elementSpanResult.innerHTML = String(resultField).trim();
+}
+
+for (let tagBtn of document.querySelectorAll(
+  '[data-btn="sign"],[data-btn="digit"]'
+)) {
+  tagBtn.addEventListener("click", (err) => insertChar(tagBtn.innerHTML));
+}
+
+for (let tagBtn of document.querySelectorAll("[data-btn-function]")) {
+  tagBtn.addEventListener("click", () => {
+    switch (tagBtn.dataset.btnFunction) {
+      case "clear":
+        clearInfo();
+        break;
+      case "erase":
+        eraseChar();
+        break;
+      case "calc":
+        getResult();
+        break;
+    }
+  });
 }
